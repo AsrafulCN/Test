6e16e756be5f25b316a1f2439edd09951ee01141
Ashraful Hoque Miraj <ashraful@comparenodes.com>
2025-03-11 12:56:37 +0600

    change website variable

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js in staging branch
-const msgForWebSite = "This is my website"
+const msgForWebSiteX = "This is my website"
 function greet() {
     console.log("Hello from Staging Environment!=stagingenv");
 }
